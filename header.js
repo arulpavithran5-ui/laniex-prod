99c8f99ab0081e4c95d4c5a56046bbfded7098fe
Arul Pavithran <arulpavithran5@gmail.com>
2026-02-12 20:57:13 +0530

    Add header component with navigation links

---
+++ header.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("header").innerHTML = `
+    <header>
+      <img src="./images/laniex-logo.png" alt="Laniex Technologies">
+      <nav>
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="projects.html">Projects</a>
+        <a href="contact.html">Contact</a>
+      </nav>
+    </header>
+  `;
+});
