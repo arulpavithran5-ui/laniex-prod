83b7254bf45ef05f889eb0f6cca949ef22c02565
Arul Pavithran <arulpavithran5@gmail.com>
2026-02-12 20:57:56 +0530

    Add footer to the page on DOMContentLoaded

---
+++ footer.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("footer").innerHTML = `
+    <footer>
+      © 2026 Laniex Technologies. All Rights Reserved.
+    </footer>
+  `;
+});
